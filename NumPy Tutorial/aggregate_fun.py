9713c830c2d6ea78f50a456b1f60dfd57609bdc8
prakash69-code <neupaneprakash9999@gmail.com>
2026-03-15 11:09:29 +0545

    Aggregate function tutorial in numpy

---
+++ NumPy Tutorial/aggregate_fun.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+arr = np.array([[10,9,8,7,6],
+                  [5,4,3,2,1]])
+
+print(np.sum(arr))
+print(np.mean(arr))
+print(np.min(arr))
+print(np.max(arr))
+print(np.argmin(arr))
+print(np.argmax(arr))
+
+print(np.sum(arr, axis=0))
+print(np.sum(arr, axis = 1))
+
